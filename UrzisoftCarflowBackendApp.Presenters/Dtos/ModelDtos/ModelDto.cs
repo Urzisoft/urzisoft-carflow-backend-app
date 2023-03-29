531ff281ceb0a455bb22eda4f231dd6e0f0b309e
Gafencu Beniamin <benigafencu@yahoo.com>
2023-03-29 18:42:51 +0300

    Adding length constraints

--- UrzisoftCarflowBackendApp.Presenters/Dtos/ModelDtos/ModelDto.cs
+++ UrzisoftCarflowBackendApp.Presenters/Dtos/ModelDtos/ModelDto.cs
@@ -5,6 +5,8 @@ namespace UrzisoftCarflowBackendApp.Presenters.Dtos.ModelDtos
     public class ModelDto
     {
         [Required]
+        [MaxLength(100)]
+        [MinLength(1)]
         public string Name { get; set; }
     }
 }
--- UrzisoftCarflowBackendApp.Presenters/Dtos/ModelDtos/ModelPatchDto.cs
+++ UrzisoftCarflowBackendApp.Presenters/Dtos/ModelDtos/ModelPatchDto.cs
@@ -1,7 +1,11 @@
-﻿namespace UrzisoftCarflowBackendApp.Presenters.Dtos.ModelDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrzisoftCarflowBackendApp.Presenters.Dtos.ModelDtos
 {
     public class ModelPatchDto
     {
+        [MaxLength(100)]
+        [MinLength(1)]
         public string Name { get; set; }
     }
 }
