b904dfbe7001ed595d1711faa1ec28116353a471
Gafencu Beniamin <benigafencu@yahoo.com>
2023-03-29 18:40:54 +0300

    Adding length constraints

--- UrzisoftCarflowBackendApp.Presenters/Dtos/CityDtos/CityDto.cs
+++ UrzisoftCarflowBackendApp.Presenters/Dtos/CityDtos/CityDto.cs
@@ -5,8 +5,13 @@ namespace UrzisoftCarflowBackendApp.Presenters.Dtos.CityDtos
     public class CityDto
     {
         [Required]
+        [MaxLength(100)]
+        [MinLength(1)]
         public string Name { get; set; }
+
         [Required]
+        [MaxLength(100)]
+        [MinLength(1)]
         public string County { get; set; }
     }
 }
--- UrzisoftCarflowBackendApp.Presenters/Dtos/CityDtos/CityPatchDto.cs
+++ UrzisoftCarflowBackendApp.Presenters/Dtos/CityDtos/CityPatchDto.cs
@@ -1,8 +1,15 @@
-﻿namespace UrzisoftCarflowBackendApp.Presenters.Dtos.CityDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrzisoftCarflowBackendApp.Presenters.Dtos.CityDtos
 {
     public class CityPatchDto
     {
+        [MaxLength(100)]
+        [MinLength(1)]
         public string Name { get; set; }
+
+        [MaxLength(100)]
+        [MinLength(1)]
         public string County { get; set; }
     }
 }
