0bc4e48f0bcf7a2516d4c096b4e30f255e2e2e57
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-04-21 22:14:46 +0300

    CFBE-4 | Updates

---
+++ UrzisoftCarflowBackendApp.UseCases/Settings/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace UrzisoftCarflowBackendApp.UseCases.Settings
+{
+    public class JwtSettings
+    {
+        public string ValidAudience { get; set; }
+        public string ValidIssuer { get; set; }
+        public string Secret { get; set; }
+    }
+}
