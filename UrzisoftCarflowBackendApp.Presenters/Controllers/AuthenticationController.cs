b8290a664ae1a9f5582fb590bccab975cfca2799
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-04-21 22:32:22 +0300

    Updates

---
+++ UrzisoftCarflowBackendApp.Presenters/Controllers/AuthenticationController.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using UrzisoftCarflowBackendApp.Presenters.Dtos.UserDtos;
+using UrzisoftCarflowBackendApp.UseCases.Users.Commands;
+
+namespace UrzisoftCarflowBackendApp.Presenters.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthenticationController: ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthenticationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            var command = new Register
+            {
+                Username = registerDto.Username,
+                Password = registerDto.Password,
+            };
+
+            var result = await _mediator.Send(command);
+
+            if (result.Status == "Error")
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> LogIn([FromBody] LoginDto logInDto)
+        {
+            var command = new Login
+            {
+                Username = logInDto.Username,
+                Password = logInDto.Password,
+            };
+
+            var result = await _mediator.Send(command);
+
+            if (result == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(result);
+        }
+    }
+}
