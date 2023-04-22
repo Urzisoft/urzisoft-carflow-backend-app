d0a18c220b3fa284479e57417dab9157e24bf05b
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-04-22 19:28:19 +0300

    Updates

--- UrzisoftCarflowBackendApp.Presenters/Dtos/CarWashStationDtos/CarWashStationPatchDto.cs
+++ UrzisoftCarflowBackendApp.Presenters/Dtos/CarWashStationDtos/CarWashStationPatchDto.cs
@@ -1,16 +1,27 @@
-﻿using UrzisoftCarflowBackendApp.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using UrzisoftCarflowBackendApp.Entities;
 
 namespace UrzisoftCarflowBackendApp.Presenters.Dtos.CarWashStationDtos
 {
     public class CarWashStationPatchDto
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+        
         public int StandardPrice { get; set; }
+        
         public int PremiumPrice { get; set; }
+
+        [Required]
         public City City { get; set; }
+
+        [Required]
         public string Address { get; set; }
+        
         public string Rank { get; set; }
+        
         public bool IsSelfWash { get; set; }
     }
 }
