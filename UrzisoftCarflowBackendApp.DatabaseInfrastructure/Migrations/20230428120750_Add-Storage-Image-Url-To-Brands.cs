888171a37f1f7928933519bf5701db652e217512
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-04-28 15:10:37 +0300

    CFBE-24 | Bring updates to AzureContainers

---
+++ UrzisoftCarflowBackendApp.DatabaseInfrastructure/Migrations/20230428120750_Add-Storage-Image-Url-To-Brands.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrzisoftCarflowBackendApp.DatabaseInfrastructure.Migrations
+{
+    public partial class AddStorageImageUrlToBrands : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StorageImageUrl",
+                table: "Brands",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StorageImageUrl",
+                table: "Brands");
+        }
+    }
+}
--- UrzisoftCarflowBackendApp.UseCases/Utils/AzureContainers.cs
+++ UrzisoftCarflowBackendApp.UseCases/Utils/AzureContainers.cs
@@ -3,10 +3,40 @@
     public class AzureContainers
     {
         private readonly static string CarFlowCarsContainer = "carflow-cars";
+        private readonly static string CarFlowBrandsContainer = "carflow-brands";
+        private readonly static string CarFlowCarServicesContainer = "carflow-carservices";
+        private readonly static string CarFlowCarWashStationsContainer = "carflow-carwashstations";
+        private readonly static string CarFlowCitiesContainer = "carflow-cities";
+        private readonly static string CarFlowGasStations = "carflow-gasstations";
 
         public static string GetCarFlowCarsContainer()
         {
             return CarFlowCarsContainer;
+        }
+
+        public static string GetCarFlowBrandsContainer()
+        {
+            return CarFlowBrandsContainer;
+        }
+
+        public static string GetCarFlowCarServicesContainer()
+        {
+            return CarFlowCarServicesContainer;
+        }
+
+        public static string GetCarFlowWashStationsContainer()
+        {
+            return CarFlowCarWashStationsContainer;
+        }
+
+        public static string GetCarFlowCitiesContainer()
+        {
+            return CarFlowCitiesContainer;
+        }
+
+        public static string GetCarFlowGasStations()
+        {
+            return CarFlowGasStations;
         }
     }
 }
