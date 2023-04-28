9824cba32b0be15c492773b7648a074a7a0e7c4e
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-04-28 15:27:00 +0300

    CFBE-24 | Add mock for update brand handler

--- UrzisoftCarflowBackendApp.UseCases/Brands/CommandHandlers/UpdateBrandHandler.cs
+++ UrzisoftCarflowBackendApp.UseCases/Brands/CommandHandlers/UpdateBrandHandler.cs
@@ -8,18 +8,23 @@ namespace UrzisoftCarflowBackendApp.UseCases.Brands.CommandHandlers
     public class UpdateBrandHandler : IRequestHandler<UpdateBrand, Brand>
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IImageStorageService _imageStorageService;
 
-        public UpdateBrandHandler(IUnitOfWork unitOfWork)
+        public UpdateBrandHandler(IUnitOfWork unitOfWork, IImageStorageService imageStorageService)
         {
             _unitOfWork = unitOfWork;
+            _imageStorageService = imageStorageService;
         }
 
         public async Task<Brand> Handle(UpdateBrand request, CancellationToken cancellationToken)
         {
             var brand = await _unitOfWork.BrandRepository.GetById(request.Id);
+            string fileName = request.Name ?? brand.Name;
+            var CustomStorageImageUrl = await _imageStorageService.UploadImage(fileName, request.File, request.ContainerName);
 
             if (brand is not null)
             {
+                brand.StorageImageUrl = CustomStorageImageUrl ?? brand.StorageImageUrl; 
                 brand.Name = request.Name ?? brand.Name;
                 brand.Description = request.Description ?? brand.Description;
 
--- UrzisoftCarflowBackendApp.UseCases/Brands/Commands/UpdateBrand.cs
+++ UrzisoftCarflowBackendApp.UseCases/Brands/Commands/UpdateBrand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using UrzisoftCarflowBackendApp.Entities;
 
 namespace UrzisoftCarflowBackendApp.UseCases.Brands.Commands
@@ -6,6 +7,8 @@ namespace UrzisoftCarflowBackendApp.UseCases.Brands.Commands
     public class UpdateBrand : IRequest<Brand>
     {
         public int Id { get; set; }
+        public IFormFile File { get; set; }
+        public string ContainerName { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
     }
