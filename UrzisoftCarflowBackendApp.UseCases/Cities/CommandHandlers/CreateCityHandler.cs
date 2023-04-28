985cdba1b5ac3429c57c57324c300ab92364a186
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-04-28 19:48:54 +0300

    Add create Car

--- UrzisoftCarflowBackendApp.UseCases/Cities/CommandHandlers/CreateCityHandler.cs
+++ UrzisoftCarflowBackendApp.UseCases/Cities/CommandHandlers/CreateCityHandler.cs
@@ -9,16 +9,22 @@ namespace UrzisoftCarflowBackendApp.UseCases.Cities.CommandHandlers
     {
 
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IImageStorageService _imageStorageService;
 
-        public CreateCityHandler(IUnitOfWork unitOfWork)
+        public CreateCityHandler(IUnitOfWork unitOfWork, IImageStorageService imageStorageService)
         {
             _unitOfWork = unitOfWork;
+            _imageStorageService = imageStorageService;
         }
 
         public async Task<City> Handle(CreateCity request, CancellationToken cancellationToken)
         {
+            string fileName = request.Name + "-" + request.County;
+            var CustomStorageImageUrl = await _imageStorageService.UploadImage(fileName, request.File, request.ContainerName);
+
             var city = new City
             {
+                StorageImageUrl = CustomStorageImageUrl,
                 Name = request.Name,
                 County = request.County,
             };
