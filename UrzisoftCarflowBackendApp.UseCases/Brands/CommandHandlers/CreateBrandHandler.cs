8101d28013f5095dc90ef886ee6f9f40d532f104
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-04-28 15:17:06 +0300

    CFBE-24 | Add mock create brand handler

--- UrzisoftCarflowBackendApp.UseCases/Brands/CommandHandlers/CreateBrandHandler.cs
+++ UrzisoftCarflowBackendApp.UseCases/Brands/CommandHandlers/CreateBrandHandler.cs
@@ -8,16 +8,22 @@ namespace UrzisoftCarflowBackendApp.UseCases.Brands.CommandHandlers
     public class CreateBrandHandler : IRequestHandler<CreateBrand, Brand>
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IImageStorageService _imageStorageService;
 
-        public CreateBrandHandler(IUnitOfWork unitOfWork)
+        public CreateBrandHandler(IUnitOfWork unitOfWork, IImageStorageService imageStorageService)
         {
             _unitOfWork = unitOfWork;
+            _imageStorageService = imageStorageService;
         }
 
         public async Task<Brand> Handle(CreateBrand request, CancellationToken cancellationToken)
         {
+            string fileName = request.Name;
+            var CustomStorageImageUrl = await _imageStorageService.UploadImage(fileName, request.File, request.ContainerName);
+
             var brand = new Brand
             {
+                StorageImageUrl = CustomStorageImageUrl,
                 Name = request.Name,
                 Description = request.Description,
             };
--- UrzisoftCarflowBackendApp.UseCases/Brands/Commands/CreateBrand.cs
+++ UrzisoftCarflowBackendApp.UseCases/Brands/Commands/CreateBrand.cs
@@ -1,10 +1,13 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using UrzisoftCarflowBackendApp.Entities;
 
 namespace UrzisoftCarflowBackendApp.UseCases.Brands.Commands
 {
     public class CreateBrand : IRequest<Brand>
     {
+        public IFormFile File { get; set; }
+        public string ContainerName { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
     }
