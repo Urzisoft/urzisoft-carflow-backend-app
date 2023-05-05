26d058d83b6e7a7f4ad99bc1cadbbd1ca68e0227
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-05-05 18:41:37 +0300

    Updates

---
+++ UrzisoftCarflowBackendApp.UseCases/Users/Commands/ChangePassword.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using UrzisoftCarflowBackendApp.Entities;
+
+namespace UrzisoftCarflowBackendApp.UseCases.Users.Commands
+{
+    public class ChangePassword : IRequest<RegisterResponse>
+    {
+        public int UserId { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
---
+++ UrzisoftCarflowBackendApp.UseCases/Users/CommandsHandlers/ChangePasswordHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using UrzisoftCarflowBackendApp.Entities;
+using UrzisoftCarflowBackendApp.UseCases.Users.Commands;
+
+namespace UrzisoftCarflowBackendApp.UseCases.Users.CommandsHandlers
+{
+    public class ChangePasswordHandler : IRequestHandler<ChangePassword, RegisterResponse>
+    {
+        public Task<RegisterResponse> Handle(ChangePassword request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
