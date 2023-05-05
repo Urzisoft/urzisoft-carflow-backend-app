cc84f7dd3ff95929d50981dacf900b817c50aa04
Gafencu Beniamin <benigafencu@yahoo.com>
2023-05-05 20:16:45 +0300

    Add database migration

---
+++ UrzisoftCarflowBackendApp.DatabaseInfrastructure/Migrations/20230505164433_Updates-UniqueConstraintsToEntites.cs
@@ -0,0 +1,414 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrzisoftCarflowBackendApp.DatabaseInfrastructure.Migrations
+{
+    public partial class UpdatesUniqueConstraintsToEntites : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Models",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "GasStation",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "GasStation",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Fuels",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cities",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "CarWashStations",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "CarWashStations",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "CarServices",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "CarServices",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LicensePlate",
+                table: "Cars",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Brands",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Models_Id",
+                table: "Models",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Models_Name",
+                table: "Models",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GasStation_Address",
+                table: "GasStation",
+                column: "Address",
+                unique: true,
+                filter: "[Address] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GasStation_Id",
+                table: "GasStation",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GasStation_Name",
+                table: "GasStation",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fuels_Id",
+                table: "Fuels",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fuels_Name",
+                table: "Fuels",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_Id",
+                table: "Cities",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_Name",
+                table: "Cities",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarWashStations_Address",
+                table: "CarWashStations",
+                column: "Address",
+                unique: true,
+                filter: "[Address] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarWashStations_Id",
+                table: "CarWashStations",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarWashStations_Name",
+                table: "CarWashStations",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarServices_Address",
+                table: "CarServices",
+                column: "Address",
+                unique: true,
+                filter: "[Address] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarServices_Id",
+                table: "CarServices",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarServices_Name",
+                table: "CarServices",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_Id",
+                table: "Cars",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_LicensePlate",
+                table: "Cars",
+                column: "LicensePlate",
+                unique: true,
+                filter: "[LicensePlate] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Brands_Id",
+                table: "Brands",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Brands_Name",
+                table: "Brands",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Models_Id",
+                table: "Models");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Models_Name",
+                table: "Models");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GasStation_Address",
+                table: "GasStation");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GasStation_Id",
+                table: "GasStation");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GasStation_Name",
+                table: "GasStation");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Fuels_Id",
+                table: "Fuels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Fuels_Name",
+                table: "Fuels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cities_Id",
+                table: "Cities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cities_Name",
+                table: "Cities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CarWashStations_Address",
+                table: "CarWashStations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CarWashStations_Id",
+                table: "CarWashStations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CarWashStations_Name",
+                table: "CarWashStations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CarServices_Address",
+                table: "CarServices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CarServices_Id",
+                table: "CarServices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CarServices_Name",
+                table: "CarServices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_Id",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_LicensePlate",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Brands_Id",
+                table: "Brands");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Brands_Name",
+                table: "Brands");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Models",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "GasStation",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "GasStation",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Fuels",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "CarWashStations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "CarWashStations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "CarServices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "CarServices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LicensePlate",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Brands",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
