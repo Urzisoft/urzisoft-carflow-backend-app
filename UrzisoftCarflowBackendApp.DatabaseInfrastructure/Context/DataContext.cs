a3408069c80fcae733edb71863c860ea560c0f72
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-05-06 22:38:19 +0300

    Updates

--- UrzisoftCarflowBackendApp.DatabaseInfrastructure/Context/DataContext.cs
+++ UrzisoftCarflowBackendApp.DatabaseInfrastructure/Context/DataContext.cs
@@ -33,8 +33,8 @@ public class DataContext : IdentityDbContext<User>
         builder.Entity<CarWashStation>().HasIndex(carWash => carWash.Address).IsUnique();
         builder.Entity<City>().HasIndex(city => city.Name).IsUnique();
         builder.Entity<Fuel>().HasIndex(fuel => fuel.Name).IsUnique();
-        // builder.Entity<GasStation>().HasIndex(gasStation => gasStation.Address).IsUnique();
-        // builder.Entity<Model>().HasIndex(model => model.Name).IsUnique();
+        builder.Entity<GasStation>().HasIndex(gasStation => gasStation.Address).IsUnique();
+        builder.Entity<Model>().HasIndex(model => model.Name).IsUnique();
 
         base.OnModelCreating(builder);
     }
--- UrzisoftCarflowBackendApp.DatabaseInfrastructure/Migrations/20230506193610_try-unique-keys-for-gas-stations-n-models.Designer.cs
+++ UrzisoftCarflowBackendApp.DatabaseInfrastructure/Migrations/20230506193610_try-unique-keys-for-gas-stations-n-models.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UrzisoftCarflowBackendApp.DatabaseInfrastructure.Context;
 
@@ -11,9 +12,10 @@ using UrzisoftCarflowBackendApp.DatabaseInfrastructure.Context;
 namespace UrzisoftCarflowBackendApp.DatabaseInfrastructure.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230506193610_try-unique-keys-for-gas-stations-n-models")]
+    partial class tryuniquekeysforgasstationsnmodels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
