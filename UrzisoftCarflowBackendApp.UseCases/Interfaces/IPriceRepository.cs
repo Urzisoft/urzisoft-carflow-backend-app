421eeacee9eba4e2252c1182822208e87f27823b
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-05-13 13:38:46 +0300

    Updates

---
+++ UrzisoftCarflowBackendApp.UseCases/Interfaces/IPriceRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrzisoftCarflowBackendApp.Entities;
+
+namespace UrzisoftCarflowBackendApp.UseCases.Interfaces
+{
+    public class IPriceRepository
+    {
+        Task Create(Brand obj);
+        Task<List<Brand>> GetAll();
+        Task<Brand> GetById(int id);
+        Task Delete(Brand obj);
+    }
+}
